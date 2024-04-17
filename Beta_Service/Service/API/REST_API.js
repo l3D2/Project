bf83f8d940d5b06aeb403617708aa059f65f40b3
BD2 <vatcharathon@gmail.com>
2024-04-17 15:55:41 +0700

    update modules

--- Beta_Service/Service/API/REST_API.js
+++ Beta_Service/Service/API/REST_API.js
@@ -1,7 +1,8 @@
-const express = require("express");
+const hyperExpress = require("hyper-express")
 const logger = require("morgan");
-const app = express();
-const { db_query } = require('../MySQL/db_module')
+const cors = require("cors");
+const app = new hyperExpress.Server();
+//const { db_query } = require('../MySQL/db_module')
 
 logger.token("client-ip", (req) => {
   return req.ip; // Fetches the client's IP address
@@ -9,8 +10,7 @@ logger.token("client-ip", (req) => {
 
 // Use Morgan for request logging with IP address
 app.use(logger(":client-ip - :method :url :status :response-time ms"));
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+//app.use(cors());
 
 const verifyAPIKey = (req, res, next) => {
   const key = req.headers["apikey"];
@@ -21,13 +21,13 @@ const verifyAPIKey = (req, res, next) => {
   }
 };
 
-app.post("/api/", verifyAPIKey, (req, res) => {
-  res.status(200).send("OK");
+app.get('/api/', verifyAPIKey, (req, res) => {
+  console.log('API Service is running');
+  res.status(200).send('API Service is running');
 });
 
 // Create DB
 app.post("/api/db_newuser/", verifyAPIKey, async (req, res) => {
-  //users.push(req.body)
   let json = req.body
   let query = "INSERT INTO Account (Account_ID, Name, Email, Permission, Token, Signup_ts) VALUES ('" + json.id + "', '" + json.username + "', 'testapi@gmail.com', 'A', 'test', '2021-01-01 00:00:00')"
   let response = await db_query(query)
@@ -52,16 +52,6 @@ app.delete("/api/db_deleteuser", verifyAPIKey, async (req, res) => {
   res.status(200).send("OK");
 })
 
-app.use(function (err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  res.status(err.status || 500);
-  res.render("error");
-});
-
-const server = app.listen(3030, function () {
-  const host = server.address().address;
-  const port = server.address().port;
-  console.log(`API listening at http://${host}:${port}`);
+app.listen(3030, () => {
+  console.log('API Service listening on port 3030');
 });
