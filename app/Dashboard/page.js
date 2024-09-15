a0fe3cd4bd1ec6f0874c0f797c444770f45060ae
Sirawit Na-ngam <sirawitnoy@gmail.com>
2024-09-15 23:40:08 +0700

    GG

--- app/Dashboard/page.js
+++ app/Dashboard/page.js
@@ -17,92 +17,95 @@ import { UserLocationContext } from "@/context/context";
 import DevicesIcon from "@mui/icons-material/Devices";
 
 export default function Dashboard() {
-  const { data: session, status } = useSession();
-  const [countDevices, setCountDevices] = useState(0);
-  const router = useRouter();
-  const { location } = useContext(UserLocationContext);
-  // Fetch device count function
-  const fetchCountDevices = async () => {
-    if (session && session.user) {
-      const id = session.user.id;
-      const res = await fetch(
-        `https://api.bd2-cloud.net/api/device/getCount/${id}`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
+    const { data: session, status } = useSession();
+    const [countDevices, setCountDevices] = useState(0);
+    const router = useRouter();
+    const { location } = useContext(UserLocationContext);
+    // Fetch device count function
+    const fetchCountDevices = async () => {
+        if (session && session.user) {
+            const id = session.user.id;
+            const res = await fetch(
+                `https://api.bd2-cloud.net/api/device/getCount/${id}`,
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+            const json = await res.json();
+            if (res.ok) {
+                setCountDevices(json);
+            } else {
+                console.error("Failed to fetch device count:");
+            }
         }
-      );
-      const json = await res.json();
-      if (res.ok) {
-        setCountDevices(json);
-      } else {
-        console.error("Failed to fetch device count:");
-      }
+    };
+
+    useEffect(() => {
+        if (session && session.user) {
+            fetchCountDevices();
+        }
+    }, [session]);
+
+    // Check status session
+    if (status === "loading") {
+        return null; // Loading indicator
     }
-  };
 
-  useEffect(() => {
-    if (session && session.user) {
-      fetchCountDevices();
+    if (!session) {
+        router.replace("/auth/signin");
+        return null;
     }
-  }, [session]);
 
-  // Check status session
-  if (status === "loading") {
-    return null; // Loading indicator
-  }
-
-  if (!session) {
-    router.replace("/auth/signin");
-    return null;
-  }
-
-  return (
-    <>
-      <Navbar session={session} />
-      <Content>
-        {"Dashboard"}
-        <div>
-          <div className="grid grid-cols-4 gap-x-2">
-            <div className="grid grid-rows-2 grid-cols-2 col-span-2 gap-2">
-              <CardStat>
-                <DevicesIcon />
-                {"Device"}
-                {countDevices}
-              </CardStat>
-              <CardStat>
-                <DevicesIcon />
-                {"Lands"}
-                {"100"}
-              </CardStat>
-              <CardStat>
-                <DevicesIcon />
-                {"Device"}
-                {"100"}
-              </CardStat>
-              <CardStat>
-                <DevicesIcon />
-                {"Lands"}
-                {"100"}
-              </CardStat>
-            </div>
-            <div className="col-span-2 row-span-2">
-              <CardFilter />
-            </div>
-          </div>
-          <div className="my-2">
-            <Divider className="bg-gray-600" style={{ height: "2px" }} />
-          </div>
-          <GoogleMapProvider>
-            <GoogleMapView location={location} />
-          </GoogleMapProvider>
-        </div>
-        <div className="grid w-full gap-2">
-          <CardDevice />
-        </div>
-      </Content>
-    </>
-  );
+    return (
+        <>
+            <Navbar session={session} />
+            <Content>
+                {"Dashboard"}
+                <div>
+                    <div className="grid grid-cols-4 gap-x-2">
+                        <div className="grid grid-rows-2 grid-cols-2 col-span-2 gap-2">
+                            <CardStat>
+                                <DevicesIcon />
+                                {"Device"}
+                                {countDevices}
+                            </CardStat>
+                            <CardStat>
+                                <DevicesIcon />
+                                {"Lands"}
+                                {"100"}
+                            </CardStat>
+                            <CardStat>
+                                <DevicesIcon />
+                                {"Device"}
+                                {"100"}
+                            </CardStat>
+                            <CardStat>
+                                <DevicesIcon />
+                                {"Lands"}
+                                {"100"}
+                            </CardStat>
+                        </div>
+                        <div className="col-span-2 row-span-2">
+                            <CardFilter />
+                        </div>
+                    </div>
+                    <div className="my-2">
+                        <Divider
+                            className="bg-gray-600"
+                            style={{ height: "2px" }}
+                        />
+                    </div>
+                    <GoogleMapProvider>
+                        <GoogleMapView location={location} />
+                    </GoogleMapProvider>
+                </div>
+                <div className="grid w-full gap-2">
+                    <CardDevice />
+                </div>
+            </Content>
+        </>
+    );
 }
